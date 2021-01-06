a8ff1b50f0ecfd21b1b7f72b7714417a2cc4b7c8
Pratika Subodh Girme <pratikagi@cybage.com>
2021-01-06 18:07:03 +0530

    json crud

---
+++ Pratika/mini project/createCategory/quiz2.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  // $.ajax({
+  //   method: "POST",
+  //   url: "db.json",
+  //   success: function (x) {},
+  //   data: {},
+  //   complete: function (data) {},
+  //   error: (e) => {
+  //     alert("Error" + e);
+  //   },
+  // });
+
+  $("#addQuestion").click(function (e) {
+    var person = {
+      id: 12,
+      question: $("#question").val(),
+      answers: {
+        a: $("#option1").val(),
+        b: $("#option2").val(),
+        c: $("#option3").val(),
+        d: $("#option4").val(),
+      },
+      correctAnswer: $("#correct").val(),
+    };
+    console.log("adding")
+    $.ajax({
+      url: "http://localhost:3000/questions/",
+      type: "POST",
+      dataType: "json",
+      contentType: "application/json",
+      success: function (data) {
+        alert("written");
+        alert(data)
+      },
+      data: JSON.stringify(person),
+    });
+    e.preventDefault();
+  });
+  $("#patch").click(function (e) {
+    $.ajax({
+      url: "http://localhost:3000/questions/3",
+      type: "PATCH",
+      dataType: "json",
+      contentType: "application/json",
+      success: function (data) {
+        alert("written");
+      },
+      data: JSON.stringify(person),
+    });
+  });
+});
