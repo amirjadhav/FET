cd4391b3235ca78d2eaec39fa566fb2c1f5230dc
amir <amirj@cybage.com>
2021-01-28 15:41:25 +0530

    java workshop

---
+++ Amir/java/workshop/casestudy/src/com/cybage/UI.java
@@ -0,0 +1,37 @@
+package com.cybage;
+
+import java.io.IOException;
+
+import com.cybage.exception.EmployeeException;
+import com.cybage.service.EmployeeService;
+import com.cybage.service.EmployeeServieImpl;
+
+public class UI {
+	public static void main(String[] args) throws Exception {
+		
+		@SuppressWarnings("unused")
+		int a = 10;
+		
+		System.out.println("employee demo...");
+		
+		EmployeeService empService = new EmployeeServieImpl();
+		
+		try {
+			//add one regular employee
+			empService.add("reg101", "reg address", 123, "REG");
+			
+			//add one retired employee
+			empService.add("ret102", "ret address", 789, "RET");
+			
+			
+			empService.displayAll();	
+			
+			empService.displayWithId(1);
+			
+			empService.delete(1);	
+		}catch(EmployeeException | IOException ee) {
+			System.err.println(ee.getLocalizedMessage());
+		}
+		
+	}
+}
