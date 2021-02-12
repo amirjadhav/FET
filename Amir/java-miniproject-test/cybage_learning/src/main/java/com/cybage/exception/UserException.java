3a8cacdb0bec8e2c6cbd46ab007a23f2e6423f91
amir <amirj@cybage.com>
2021-02-12 17:03:25 +0530

    java mini

--- Amir/java-miniproject-test/cybage_learning/src/main/java/com/cybage/exception/UserException.java
+++
@@ -1,9 +0,0 @@
-package com.cybage.exception;
-
-import javax.servlet.ServletException;
-
-public class UserException extends ServletException{
-	public UserException(String msg) {
-		super(msg);
-	}
-}
