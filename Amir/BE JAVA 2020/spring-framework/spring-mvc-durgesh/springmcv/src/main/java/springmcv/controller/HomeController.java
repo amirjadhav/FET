9c1240f8ddbf72ef261da2fd5bac6ad37a5e8740
amir <amirj@cybage.com>
2021-02-15 20:09:53 +0530

    spring mvc durgesh

---
+++ Amir/BE JAVA 2020/spring-framework/spring-mvc-durgesh/springmcv/src/main/java/springmcv/controller/HomeController.java
@@ -0,0 +1,16 @@
+package springmcv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(path="/index",method=RequestMethod.GET)
+	
+	public String home() {
+		System.out.print("Into home");
+		return "index";
+	}
+}
