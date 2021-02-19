026b6166c853b60f87916dc31b8d1273c57c7e7d
Amir <jadhav.amir100@gmail.com>
2021-02-19 23:28:32 +0530

    workshop2 rest controller

--- Amir/java/spring-framework/spring-workspace/workshop-2/src/main/java/com/example/ProductController.java
+++ Amir/java/spring-framework/spring-workspace/workshop-2/src/main/java/com/example/ProductController.java
@@ -4,66 +4,45 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/products")
 public class ProductController {
 	@Autowired
 	ProductService productService;
 
 	@RequestMapping()
-	public ModelAndView getProducts() {
-		ModelAndView model = new ModelAndView("/index");
-		List<Product> product = productService.findAll();
-		model.addObject("product", product);
-		return model;
+	public List<Product> getProducts() {
+		return productService.findAll();
 	}
 
 	@RequestMapping(path = "/{id}", method = RequestMethod.GET)
-	public String getProductById(@PathVariable int id) {
-		Optional<Product> product = productService.findById(id);
-		System.out.println(product);
-		return "index";
+	public Optional<Product> getProductById(@PathVariable int id) {
+		return productService.findById(id);
+
 	}
 
-	@RequestMapping(path = "/add")
-	public ModelAndView addProduct(Model model) {
-		ModelAndView mv = new ModelAndView("add-product");
-		Product product = new Product();
-		mv.addObject("product", product);
-		return mv;
+	@RequestMapping(path = "/add", method = RequestMethod.POST)
+	public Product addProduct(@RequestBody Product product) {
+		return productService.save(product);
 	}
 
-	@RequestMapping(path = "/submit-product", method = RequestMethod.POST)
-	public String submitProduct(@ModelAttribute("product") Product product) {
-		Product p = productService.save(product);
-		System.out.println("Product submitted : " + p);
-		return "redirect:/products";
+	@RequestMapping(path = "/edit/{id}", method = RequestMethod.PUT)
+	public Product updateProduct(@RequestBody Product product, @PathVariable int id) {
+		return productService.update(product);
 	}
 
-	@RequestMapping(path = "/edit/{id}", method = RequestMethod.GET)
-	public ModelAndView updateProduct(@PathVariable int id) {
-		System.out.println("inside edit product");
-		ModelAndView mv = new ModelAndView("edit-product");
-		Product product = productService.findById(id).get();
-		System.out.println("optonal " + product);
-		mv.addObject("product", product);
-		return mv;
-	}
-
-	@RequestMapping(path = "/delete/{id}")
-	public String deleteProductById(@PathVariable int id) {
+	@RequestMapping(path = "/delete/{id}", method = RequestMethod.DELETE)
+	public void deleteProductById(@PathVariable int id) {
+		System.out.println("id:" + id);
 		productService.deleteById(id);
 		System.out.println("Product deleted...");
-		return "redirect:/products";
 	}
 
 }
